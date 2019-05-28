6342b12b76d0166e7d08ca5de1cab8e41391ffbe
Luiz Paulo <32080340+LuizPauloPereira@users.noreply.github.com>
2019-05-27 21:45:16 -0400

    Add files via upload

---
+++ Controllers/FuncaoController.cs
@@ -0,0 +1,37 @@
+﻿using AULADAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AULADAL.Controllers
+{
+    public class FuncaoController : Controller
+    {
+        EMPRESAEntities ctx = new EMPRESAEntities();
+
+        // GET: Funcao
+        public ActionResult Index()
+        {            
+            return View(ctx.FUNCAO.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FUNCAO funcao)
+        {
+            if(ModelState.IsValid)
+                {
+                    ctx.FUNCAO.Add(funcao);
+                    ctx.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            return View(funcao);
+        }
+    }
+}
---
+++ Models_MD/FuncaoMD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AULADAL.Models
+{
+    [MetadataType(typeof(FuncaoMD))]
+    public partial class FUNCAO { }
+
+    public partial class FuncaoMD
+    {
+        [DisplayName("Código")]
+        public int FuncaoId { get; set; }
+
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório")]
+        [StringLength(5, ErrorMessage ="Máximo 5 caractéres")]
+        public string Sigla { get; set; }
+    }
+}
